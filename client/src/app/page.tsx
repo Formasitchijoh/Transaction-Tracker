c9dd5e560c717d58a3892c8b9f8068437d669a84
Formasitchijoh <formasitf@gmail.com>
2025-02-10 09:58:02 +0100

    fix: ui of the front end

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,31 @@
+
+import Image from "next/image";
 import TransactionPage from "./transactions/page";
 
 export default function Home() {
   return (
       <main className="flex">
-      <div className="flex w-full">
-        <nav className="w-64 p-4 h-screen bg-gray-600 sticky top-0">
-          <div className="flex justify-center items-center">
-            <h1 className="text-md py-2 text-white font-bold hidden md:block">Tracky</h1>
+      <div className="md:flex w-full">
+        <nav className="w-64 p-4 md:h-screen hidden md:block bg-purple-900 sticky top-0">
+          <div className="flex justify-center cursor-wait items-center">
+            <h1 className=" text-2xl md:text-4xl tracking-wide leading-normal py-2 text-white font-bold hidden md:block">Tracky</h1>
+            <Image src={"/nano.png"} height={100} width={50} className="rounded-full aspect-auto"  alt={"nano-dev"}/>
           </div>
-          <ul className="flex flex-col text-white">
-            <li className="text-lg font-bold py-2">
+          <hr className="my-4 border-gray-500"/>
+          <ul className="flex cursor-pointer flex-col justify-center items-start text-white">
+            <li className="text-lg  border-1 block px-4 hover:rounded w-full hover:bg-purple-600 rounded-sm font-bold py-2">
              <span>Transactions</span>
             </li>
-       
           </ul>
         </nav>
-        <div className="w-full p-8">
+        <div className="w-full">
+        <div className="flex justify-center md:hidden sticky top-0 bg-purple-900 h-[20vh] cursor-wait items-center">
+            <h1 className=" text-2xl md:text-4xl tracking-wide leading-normal py-2 text-white font-bold">Tracky</h1>
+            <Image src={"/nano.png"} height={100} width={50} className="rounded-full aspect-auto"  alt={"nano-dev"}/>
+          </div>
+          <div className="p-8">
         <TransactionPage/>
+          </div>
         </div>
       </div>
     </main>
